4acdbe7eeccba164b2bfbf519c4a86f52cf612b1
Ahmad Reza Cheraghi <ahmad.cheraghi@uni-duesseldorf.de>
2019-03-11 12:02:34 +0100

    Update two_particles_tiles_locations.py

--- scenario/two_particles_tiles_locations.py
+++ scenario/two_particles_tiles_locations.py
@@ -2,7 +2,7 @@
 A sim is created that has two particles, two locations, and two tiles.
 """
 
-def create_scenario(sim):
+def scenario(sim):
     sim.add_particle(0, 0)
     sim.add_particle(1, 0)
     sim.add_location(0.5, 1)
