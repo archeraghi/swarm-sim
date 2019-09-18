aaef06392717f942a9d96d21ef919cf1708b831f
Ahmad Reza Cheraghi <ahmad.cheraghi@uni-duesseldorf.de>
2019-09-18 10:57:09 +0200

    Update random_walk.py

--- solution/random_walk.py
+++ solution/random_walk.py
@@ -3,7 +3,5 @@ import random
 from solution.std_lib import *
 
 def solution(world):
-
-    if world.get_actual_round() % 1 == 0:
-        for particle in world.get_particle_list():
-            particle.move_to(random.choice(direction))
+    for particle in world.get_particle_list():
+        particle.move_to(random.choice(direction))
